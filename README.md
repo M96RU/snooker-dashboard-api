ed187b20a1acdf8d8e6304fd9c41a1dc68302c7c
Manuel RUSSO <manuel.russo@atos.net>
2018-10-25 23:15:46 +0200

    login with spring security

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # snooker-dashboard-api
+https://spring.io/guides/gs/securing-web/
+https://spring.io/guides/tutorials/spring-security-and-angular-js/
+
+# backend jwt
+https://medium.com/@nydiarra/secure-a-spring-boot-rest-api-with-json-web-token-reference-to-angular-integration-e57a25806c50
+
+# angular jwt
+https://blog.angular-university.io/angular-jwt-authentication/
+https://ryanchenkie.com/angular-authentication-using-the-http-client-and-http-interceptors
--- src/main/java/p/russo/manuel/snookerdashboardapi/SnookerDashboardApiApplication.java
+++ src/main/java/p/russo/manuel/snookerdashboardapi/SnookerDashboardApiApplication.java
@@ -2,9 +2,32 @@ package p.russo.manuel.snookerdashboardapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @SpringBootApplication
 public class SnookerDashboardApiApplication {
+
+	@Configuration
+	@Order(SecurityProperties.IGNORED_ORDER)
+	protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http
+					.httpBasic()
+					.and()
+					.authorizeRequests()
+					.antMatchers("/index.html", "/", "/home", "/login").permitAll()
+					.anyRequest().authenticated()
+					.and().logout().logoutUrl("/api/logout").permitAll()
+					.and().csrf()
+					.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+		}
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SnookerDashboardApiApplication.class, args);
